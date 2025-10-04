57b3a3552dbbeb5d38d8b208a65f8c558a24d874
Saira Gulamali <sguacamole98@gmail.com>
2025-10-04 15:17:10 +0100

    add primary font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-blue-600 text-7xl bg-yellow-200 sm:bg-blue-300 md:bg-amber-300 lg:bg-purple-300 lg:text-8xl lg:font-bold">
+    <div className="text-blue-600 text-7xl bg-yellow-200 sm:bg-blue-300 md:bg-amber-300 lg:bg-purple-300 lg:text-8xl lg:font-bold font-primary">
       hello
     </div>
   );
