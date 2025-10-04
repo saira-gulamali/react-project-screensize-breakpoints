084ba609dfd4dd810129326c2315598c95328fb4
Saira Gulamali <sguacamole98@gmail.com>
2025-10-04 15:51:09 +0100

    add xl screen size

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-black text-7xl sm:bg-blue-300 md:bg-amber-300 lg:bg-purple-300 lg:text-8xl lg:font-bold font-primary h-screen flex items-center justify-center">
+    <div className="text-black text-7xl sm:bg-blue-300 md:bg-amber-300 lg:bg-purple-300 lg:text-8xl lg:font-bold xl:bg-gray-400 font-primary h-screen flex items-center justify-center">
       hello
     </div>
   );
