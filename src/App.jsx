cf20a76d357d2fdcf0129921a1899e987ef206a3
Saira Gulamali <sguacamole98@gmail.com>
2025-10-04 15:00:07 +0100

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-blue-600 text-7xl bg-yellow-200 sm:bg-blue-300 md:bg-amber-300 lg:bg-purple-300 lg:text-8xl lg:font-bold">
+      hello
+    </div>
+  );
+}
+
+export default App;
